7c688accb2bb715d523471370422079deb2110a6
inf4385 <inf4385@hs-worms.de>
2025-06-09 13:29:46 +0200

    logout, messages and little changes

--- lib/widgets/hamburger_menu.dart
+++ lib/widgets/hamburger_menu.dart
@@ -1,66 +1,78 @@
 import 'package:flutter/material.dart';
-import '../screens/login_screen.dart';
-import '../screens/account_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
+import '../screens/account_screen.dart';
+import '../screens/login_screen.dart';
+
 class HamburgerMenu extends StatelessWidget {
-  const HamburgerMenu(
-      {super.key}); // <- const ist erlaubt, weil keine Felder drin sind
+  const HamburgerMenu({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return PopupMenuButton<String>(
-      color: Colors.white,
-      icon: const Icon(Icons.menu, color: Colors.white),
-      itemBuilder: (BuildContext context) {
-        return [
-          const PopupMenuItem<String>(
-            value: 'konto',
-            child: Text('Konto', style: TextStyle(color: Color(0xFF122620))),
-          ),
-          const PopupMenuItem<String>(
-            value: 'favoriten',
-            child:
-                Text('Favoriten', style: TextStyle(color: Color(0xFF122620))),
-          ),
-          const PopupMenuItem<String>(
-            value: 'darkmode',
-            child: Text('Darkmode', style: TextStyle(color: Color(0xFF122620))),
-          ),
-          const PopupMenuDivider(),
-          const PopupMenuItem<String>(
-            value: 'login',
-            child: Text('Anmelden / Registrieren',
-                style: TextStyle(color: Color(0xFF122620))),
-          ),
-        ];
-      },
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        final isLoggedIn = snapshot.hasData;
 
-      // Aktionen bei Auswahl
-      onSelected: (value) {
-        if (value == 'konto') {
-          final user = FirebaseAuth.instance.currentUser;
-          final isLoggedIn = user != null;
-
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) =>
-                  isLoggedIn ? const AccountScreen() : const LoginScreen(),
+        return PopupMenuButton<String>(
+          color: Colors.white,
+          icon: const Icon(Icons.menu, color: Colors.white),
+          onSelected: (value) {
+            if (value == 'konto') {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) =>
+                      isLoggedIn ? const AccountScreen() : const LoginScreen(),
+                ),
+              );
+            } else if (value == 'favoriten') {
+              // TODO: Favoriten anzeigen
+            } else if (value == 'darkmode') {
+              // TODO: Darkmode toggeln
+            } else if (value == 'login') {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const LoginScreen()),
+              );
+            } else if (value == 'logout') {
+              FirebaseAuth.instance.signOut();
+              ScaffoldMessenger.of(context).showSnackBar(
+                const SnackBar(
+                  backgroundColor: Color(0xFF2E6B4D),
+                  content: Text(
+                    'Erfolgreich ausgeloggt',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              );
+            }
+          },
+          itemBuilder: (context) => [
+            const PopupMenuItem<String>(
+              value: 'konto',
+              child: Text('Konto', style: TextStyle(color: Color(0xFF122620))),
             ),
-          );
-        } else if (value == 'favoriten') {
-          // TODO: Navigation zu Favoriten
-        } else if (value == 'darkmode') {
-          // TODO: Darkmode umschalten
-        } else if (value == 'login') {
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => const LoginScreen(),
+            const PopupMenuItem<String>(
+              value: 'favoriten',
+              child:
+                  Text('Favoriten', style: TextStyle(color: Color(0xFF122620))),
             ),
-          );
-        }
+            const PopupMenuItem<String>(
+              value: 'darkmode',
+              child:
+                  Text('Darkmode', style: TextStyle(color: Color(0xFF122620))),
+            ),
+            const PopupMenuDivider(),
+            PopupMenuItem<String>(
+              value: isLoggedIn ? 'logout' : 'login',
+              child: Text(
+                isLoggedIn ? 'Logout' : 'Anmelden / Registrieren',
+                style: const TextStyle(color: Color(0xFF122620)),
+              ),
+            ),
+          ],
+        );
       },
     );
   }
